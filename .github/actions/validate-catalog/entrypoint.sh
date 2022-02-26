4c0d4c12cc64faf0d35f240809eae0addc2b54b9
Justin Kolberg <amd.prophet@gmail.com>
2022-02-25 16:03:48 -0800

    simplify gh workflow, implement validation
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>

--- .github/actions/validate-catalog/entrypoint.sh
+++ .github/actions/validate-catalog/entrypoint.sh
@@ -9,9 +9,5 @@ catalog_api_url="https://github.com/sensu/catalog-api/releases/download/${catalo
 echo "URL: ${catalog_api_url}"
 curl -L "$catalog_api_url" | gunzip -c | tar -x -C /usr/bin
 
-
-echo "Creating tmp directory for generated files"
-mkdir -p ./tmp
-
-echo "Generating Catalog API"
-catalog-api catalog generate --temp-dir "./tmp"
+echo "Validating Catalog"
+catalog-api catalog validate

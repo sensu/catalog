0880c0b1cdcaf11be8fa8f51e45c3cf5d47210a2
Justin Kolberg <amd.prophet@gmail.com>
2022-03-09 14:41:13 -0800

    set s3 objects to public readable
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>

--- .github/actions/publish-catalog-api/entrypoint.sh
+++ .github/actions/publish-catalog-api/entrypoint.sh
@@ -5,6 +5,7 @@ echo "Publishing Catalog API"
 aws s3 cp "$1" "s3://${AWS_S3_BUCKET}" \
     --recursive \
     --exclude version.json \
+    --acl 'public-read' \
     --cache-control 'max-age=31104000' # 360 days
 
 aws s3 cp "${1}/version.json" "s3://${AWS_S3_BUCKET}" \

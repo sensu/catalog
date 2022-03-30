f1ee4b9673ad48e51a921ffb978e18fa100f671b
c-kruse <ctkruse99@gmail.com>
2022-03-30 15:05:52 -0700

    Exit vet on error

--- vet.sh
+++ vet.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 cue vet :catalog -d "#IntegrationSpec" $(find integrations -name "sensu-integration.yaml" )
 cue vet :catalog -d "#TypeWrapper" $(find integrations -name "sensu-resources.yaml" )
